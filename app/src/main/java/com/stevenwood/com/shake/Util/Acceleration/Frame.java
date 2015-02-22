5ad6c9769ae2d603dfc630953edde99bd92560e3
Hunter John Larco <hjlarco@gmail.com>
2015-02-22 12:11:33 -0500

    Shake Detection Algorithm

---
+++ app/src/main/java/com/stevenwood/com/shake/Util/Acceleration/Frame.java
@@ -0,0 +1,24 @@
+package Acceleration;
+
+public class Frame {
+
+	private int start;
+	private int end;
+	
+	public Frame(int start, int end){
+		this.start = start;
+		this.end = end;
+	}
+	
+	public int getStart(){
+		return start;
+	}
+	public int getEnd(){
+		return end;
+	}
+	
+	public String toString(){
+		return "{end="+end+", start="+start+"}";
+	}
+	
+}
---
+++ app/src/main/java/com/stevenwood/com/shake/Util/Acceleration/Point.java
@@ -0,0 +1,28 @@
+package Acceleration;
+
+public class Point {
+
+	private double mag;
+	private long time;
+	
+	public Point(double mag, long timestamp){
+		this.mag = mag;
+		time = timestamp;
+	}
+	public Point(float x, float y, float z, long timestamp){
+		mag = Math.sqrt(x*x + y*y + z*z);
+		time = timestamp;
+	}
+	
+	public double getMagnitude(){
+		return mag;
+	}
+	public long getTimestamp(){
+		return time;
+	}
+	
+	public String toString(){
+		return "{mag="+mag+", time="+time+"}";
+	}
+	
+}
